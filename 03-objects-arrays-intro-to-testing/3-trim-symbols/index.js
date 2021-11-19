079507a55fbd7437f2ce9de6efa4317a0301da85
d.krotchik <d.krotchik@bftcom.com>
2021-11-19 12:20:47 +0300

    Third task

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  const toArr = string.split('');
+  const groupedArr = [];
+  toArr.forEach(el => {
+    let lastItem = groupedArr[groupedArr.length - 1];
+    if (!lastItem) {
+      groupedArr.push(el);
+    } else {
+      const lastLetter = lastItem[lastItem.length - 1];
+      if (lastLetter === el) {
+        lastItem += el;
+        groupedArr[groupedArr.length - 1] = lastItem;
+      } else {
+        groupedArr.push(el);
+      }
+    }
+  });
+  const result = [];
+  groupedArr.forEach(el => {
+    if (el.length > size) {
+      result.push(el.substr(0, size));
+    } else {
+      result.push(el);
+    }
+  });
+  return result.join('');
 }
