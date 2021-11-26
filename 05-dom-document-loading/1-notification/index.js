f0afb25474332d388fca5cb5a96eae7541c0d38a
d.krotchik <d.krotchik@bftcom.com>
2021-11-26 12:00:52 +0300

    Fifth task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,45 @@
 export default class NotificationMessage {
+  element;
 
+  constructor(message = '', { duration = 0, type = ''} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+  }
+
+  show(target = document.body) {
+    target.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.element.remove();
+  }
 }
